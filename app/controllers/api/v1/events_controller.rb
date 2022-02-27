7fb3d0c650aa4f759cb3034b2a202dcc7fd33150
Pascal Racine-Venne <pascalracinevenne@gmail.com>
2022-02-27 15:36:48 -0500

    Tested #index and #create with postman, everything is working smoothly

---
+++ app/controllers/api/v1/events_controller.rb
@@ -0,0 +1,37 @@
+
+class Api::V1::EventsController  < ApplicationController
+  protect_from_forgery with: :null_session
+
+  # GET /api/v1/events
+  def index
+    events = Event.all
+    render json: serializer(events)
+  end
+
+  # POST /api/v1/events
+  def create
+    event = Event.new(event_params)
+    if event.save
+      render json: serializer(event)
+    else
+      render json: errors(event), status: 422
+    end
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:title, :description, :start_time, :end_time)
+  end
+
+  def serializer(records)
+    EventSerializer
+      .new(records)
+      .serializable_hash
+      .to_json
+  end
+
+  def errors(record)
+    { errors: record.errors.messages }
+  end
+end
