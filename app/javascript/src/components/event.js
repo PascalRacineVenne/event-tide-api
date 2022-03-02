136738b51a28d30a1947a70d5b18ea2428c9a7e8
Pascal Racine-Venne <pascalracinevenne@gmail.com>
2022-03-02 11:50:02 -0500

    added styled components

--- app/javascript/src/components/event.js
+++ app/javascript/src/components/event.js
@@ -7,20 +7,20 @@ const Event = ({title, description, start_time, end_time}) => {
     const dayName = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
 
     const moment = new Date(time),
-    year = moment.getFullYear().toString(),
-    // month = ("0" + (moment.getMonth() + 1)).slice(-2),
-    month = monthName[moment.getMonth()],
-    date = ("0" + moment.getDate()).slice(-2),
-    day = dayName[moment.getDay()],
-    hours = ("0" + moment.getHours()).slice(-2),
-    minutes = ("0" + moment.getMinutes()).slice(-2)
+      year = moment.getFullYear().toString(),
+      month = monthName[moment.getMonth()],
+      date = ("0" + moment.getDate()).slice(-2),
+      day = dayName[moment.getDay()],
+      hours = ("0" + moment.getHours()).slice(-2),
+      minutes = ("0" + moment.getMinutes()).slice(-2)
+
     return {
-      year: year,
-      month: month,
-      date: date,
-      day: day,
-      hours: hours,
-      minutes: minutes
+        year: year,
+        month: month,
+        date: date,
+        day: day,
+        hours: hours,
+        minutes: minutes
     };
   } 
 
